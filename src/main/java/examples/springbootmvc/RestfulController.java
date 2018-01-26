09e2d352f2dd8791449cceeda899ec43a886beb5
root <root@localhost.localdomain>
2018-01-26 22:51:56 +0800

    multiconfig

--- src/main/java/examples/springbootmvc/RestfulController.java
+++ src/main/java/examples/springbootmvc/RestfulController.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest")
 public class RestfulController {
 	
+	@Value("${env}")
+	private String env;
+	
 	
 	@GetMapping("/get")
 	public RestResponse get(){
@@ -21,7 +25,7 @@ public class RestfulController {
 		UserInfo u = new UserInfo();
 		u.setUserId("11111");
 		u.setTimeStamp(new Date());
-		u.setInfo("this is release 5");
+		u.setInfo("this is release 5 env:"+env);
 		
 		RestResponse res = new RestResponse();
 		res.setRetCode(0);
